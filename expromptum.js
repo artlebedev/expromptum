bc53e2bbc7205e9fef979073864fef2acb3b901f
Vyacheslav Aristov <aristov@artlebedev.ru>
2018-06-26 17:13:32 +0300

    Refactor `html` code

--- expromptum.js
+++ expromptum.js
@@ -1077,27 +1077,20 @@ xP.controls.register({
     base : '_item',
     prototype : {
         element_selector : '.xp_html',
-
         init : function(params) {
-            xP.controls.html.base.init.apply(this, arguments);
-
-            this.init_locale(params);
+            xP.controls.html.base.init.apply(this, arguments)
+            this.init_locale(params)
         },
-
         val : function(value) {
-            if(!arguments.length) {
-                return this.disabled? undefined : this.$element.html();
+            if(arguments.length) {
+                this.$element.html(value)
+                this.change()
+                return this
             }
-            else {
-                this.$element.html(value);
-
-                this.change();
-
-                return this;
-            }
+            else return this.disabled? undefined : this.$element.html()
         }
     }
-});
+})
 
 
 /***/ }),
